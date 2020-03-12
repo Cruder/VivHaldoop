72434b0261e8df6192496f2a46f69f2f375008ed
Barogthor <f.thery3103@gmail.com>
2020-03-12 15:19:02 +0100

    fix app properties

--- src/main/scala/com/cruder/vivhaldoop/share/AppProperties.scala
+++ src/main/scala/com/cruder/vivhaldoop/share/AppProperties.scala
@@ -1,18 +1,22 @@
 package com.cruder.vivhaldoop.share
 
-import java.io.{FileInputStream, IOException, File}
+import java.io.{File, FileInputStream, IOException}
 import java.util.Properties
 
 object AppProperties {
   private def init = () => {
 
+    var input: FileInputStream = null
     val prop = new Properties()
     try {
       val properties = new File("app.properties")
-      prop.load(new FileInputStream(properties))
+      input = new FileInputStream(properties)
+      prop.load(input)
     } catch {
       case io: IOException =>
         io.printStackTrace()
+    } finally {
+      if (input != null) input.close()
     }
     prop
   }
